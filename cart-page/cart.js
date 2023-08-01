642512211724eb797f9ba34a39b05fd80cb667b1
cuongdotuan <dotuancuong911@gmail.com>
2023-08-01 22:05:12 +0700

    add cart-page, fix product-page, fix home-page

---
+++ cart-page/cart.js
@@ -0,0 +1,100 @@
+let CART = "CART"
+
+
+function formatPrice(price) {
+    return price.toLocaleString('vi-VN');
+}
+function renderCart() {
+    let totalPrice = 0
+    let result = getCartLocalStorage()
+    if (result === null) return
+    document.querySelector('.list-item').innerHTML = ''
+    for (let item of result) {
+
+        let { id, name, price, img, quantity } = item
+        let totalPriceOfOne = price * quantity
+        totalPrice = totalPrice + totalPriceOfOne
+        let tr = document.createElement('tr')
+
+        tr.innerHTML = `
+            <td class="d-flex" style="align-items: center;">
+                <i class="fa-solid fa-circle-xmark" style="color: #ee4a4a; padding-right: 8px; cursor: pointer;"></i>
+                <div class="ava">
+                    <img src="${img}" alt="" width = "100">
+                </div>
+            </td>
+                
+            <td>
+                <div>
+                        <b>${name}</b>
+                </div>
+            </td>
+
+            <td class="text-center">
+                <div>
+                    <p>${formatPrice(price)}</p>
+                </div>
+            </td>
+
+            <td class="quantity-column text-center">
+                <div class="d-flex quantity">
+                    <i class="fa-solid fa-minus" onclick="decreaseQuantity(${id})"></i><p>${quantity}</p><i class="fa-solid fa-plus" onclick="increaseQuantity(${id})"></i>
+                </div>
+            </td>
+
+            <td class="text-center">
+                <div>
+                    <b>${formatPrice(totalPriceOfOne)}đ</b>
+                </div>
+            </td>
+        `
+
+        document.querySelector('.list-item').appendChild(tr)
+    }
+    document.getElementById('totalPrice').innerHTML = `
+    <div class="d-flex" style="align-items: center;justify-content: end; padding-bottom: 12px">
+        <p style="margin-right:8px">Tổng tiền:</p> <b style="font-size: 18px">${formatPrice(totalPrice)}đ</b>
+    </div>`
+
+}
+
+function getCartLocalStorage() {
+    let cartData = localStorage.getItem(CART)
+    try {
+        let products = JSON.parse(cartData)
+        return products
+    } catch (error) {
+        return null
+    }
+}
+function decreaseQuantity(id) {
+    
+    let result = getCartLocalStorage()
+    if (result === null) return
+    let item = result.find(function (x) {
+        return x.id === id.toString()
+    })
+    if(item === null)return
+    if (item.quantity < 2) {
+        alert('Lỗi số lượng sản phẩm')
+        return
+    }
+    item.quantity = item.quantity - 1
+    localStorage.setItem(CART, JSON.stringify(result))
+    renderCart()
+}
+
+function increaseQuantity(id) {
+    let result = getCartLocalStorage()
+    if (result === null) return
+    let item = result.find(function (x) {
+        return x.id === id.toString()
+    })
+    if(item === null)return
+    item.quantity = item.quantity + 1
+    localStorage.setItem(CART, JSON.stringify(result))
+    renderCart()
+
+}
+
+renderCart()
--- products/products.js
+++ products/products.js
@@ -99,7 +99,7 @@ async function renderProducts(products) {
         <div>
             <div class="image" style="background-image: url(${imgs[0]});"></div>
             <h4><a href="#" onclick="detailProduct(${id})"></a>${name}</h4>
-            <p>${type}</p>
+        
             <p>${formatPrice(price)}đ</p>
         </div>
         `
@@ -199,28 +199,51 @@ function filterByWallet() {
     fetchData(filterParams)
 }
 
+let quantityValue = 1
 async function detailProduct(id) {
+    quantityValue = 1
     document.querySelector('.listItem').innerHTML = ''
     let product = null
     try {
         const res = await fetch(apiUrl + endPoint.products + "/" + id)
         product = await res.json()
         console.log(product)
-        
+
     } catch (error) {
         console.log(error)
-        
+
     }
 
-    if(product === null) return
+    if (product === null) return
 
-    let {imgs, name, price, stock} = product
+    let { imgs, name, price, type, stock } = product
+
+    let imageArr = imgs.map(function (item) {
+        return `<img onclick='changeImg("${item}")' src=${item} width='100' class='imagePreview' >`
+    })
+    let imageString = imageArr.join('')
+
+    console.log(1, product)
+
+    const buyButton = stock === 0
+        ? `<div class="SoldOutBtn"><p>----Sold out----</p></div>`
+        : `<div class="addBtn"><p>Add to cart</p></div>`
+
     document.querySelector('.container').innerHTML = `
-    <div class="detailPage">
-        <div class="detailImage col-6"></div>
+    <div class="backToPrd"><a href="products.html">Back To Products </a></div>
+    <div class="detail">
+
+        <div class="detailImage col-6">
+            <div class="avatar"><img src=${imgs[0]}></div>
+
+            <div class="preview">${imageString}</div>
+        </div>
+
+        
         <div class="information col-6">
             <h2 style="padding-bottom: 10px">${name}</h2>
             <h4 style="padding-bottom: 24px">${formatPrice(price)} đ</h4>
+            <p>Loại: ${type}</p>
             <ul style="padding-bottom: 24px; line-height: 28px">
                 <li>Chất liệu: Vải Polyester cao cấp chống thấm nước<li>
                 <li>Nhiều ngăn nhỏ đựng đồ cá nhân<li>
@@ -229,17 +252,105 @@ async function detailProduct(id) {
             
             <div class="add">
                 <div class="quantity">
-                    <button><i class="fa-solid fa-minus" style="color: #000000;"></i></button>1<button><i class="fa-solid fa-plus" style="color: #000000;"></i></button>
+                    <button class="decrease" onclick="decreaseInputNumber()"><i class="fa-solid fa-minus" style="color: #000000;"></i></button>
+                    <span class="quantityNumber">${quantityValue}</span>
+                    <button class="increase" onclick="increaseInputNumber()"><i class="fa-solid fa-plus" style="color: #000000;"></i></button>
                 </div>
-                <div class="addBtn"><p>Add to cart</p></div>
+                ${buyButton}
             </div>
-            <div class="buyBtn"><p>Buy it now</p></div>
+            
         </div>
     </div>
     
     `
-    
+    document.querySelector(".addBtn").addEventListener("click", function () {
+        addToCart(product)
+    })
+}
+
+function changeImg(url) {
+    let newAvatar = document.querySelector('.avatar')
+    newAvatar.innerHTML = `
+    <img src=${url} width="100%">
+    `
+}
+
+function decreaseInputNumber() {
+
+    --quantityValue
+    if (quantityValue < 1) {
+        alert('Lỗi số lượng sản phẩm')
+        return
+    }
+
+    document.querySelector('.quantityNumber').innerHTML = quantityValue
+    console.log(quantityValue)
 
 
 }
+
+function increaseInputNumber() {
+    ++quantityValue
+    document.querySelector('.quantityNumber').innerHTML = quantityValue
+    console.log(quantityValue)
+
+}
+
+const CART = 'CART'
+
+function addToCart(data) {
+    let { id, name, price, imgs } = data
+    console.log(data)
+    const currentCart = localStorage.getItem(CART)
+    if (!currentCart) {
+        console.log("check")
+        const newItem = {
+            id: id,
+            name: name,
+            price: price,
+            img: imgs[0],
+            quantity: quantityValue
+        }
+        const cart = [newItem]
+
+        localStorage.setItem(CART, JSON.stringify(cart))
+    }
+    else {
+        try {
+            const parsedCart = JSON.parse(currentCart)
+            const cart = parsedCart
+            const exitsItem = cart.find(i => i.id === id)
+            if (!exitsItem) {
+                const newItem = {
+                    id: id,
+                    name: name,
+                    price: price,
+                    img: imgs[0],
+                    quantity: quantityValue
+                }
+                cart.push(newItem)
+                localStorage.setItem(CART, JSON.stringify(cart))
+            }
+            else {
+                exitsItem.quantity = exitsItem.quantity + quantityValue
+                localStorage.setItem(CART, JSON.stringify(cart))
+
+            }
+
+        } catch (error) {
+            localStorage.removeItem(CART)
+            const newItem = {
+                id: id,
+                name: name,
+                price: price,
+                img: imgs[0],
+                quantity: quantityValue
+            }
+            const cart = [newItem]
+            localStorage.setItem(CART, JSON.stringify(cart))
+        }
+
+    }
+}
+
 document.querySelector('.container').appendChild(loading.list())
